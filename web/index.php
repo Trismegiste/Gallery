52b5f389b0285d58473a5cafd01a5c4f3cf6f0fe
Trismegiste <florent.g@allopneus.com>
2013-06-15 13:21:46 +0200

    bootsrap w/ cache

--- web/index.php
+++ web/index.php
@@ -7,9 +7,10 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $app = new Trismegiste\AppKernel();
 
 $app['debug'] = true;
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
+$app->register(new Silex\Provider\TwigServiceProvider(), [
     'twig.path' => dirname(__DIR__) . '/views',
-));
+    'twig.options' => ['cache' => dirname(__DIR__) . '/cache']
+]);
 
 $app->get('/', 'Trismegiste\Controller::home');
 $app->run();
