f4bf9bb78d5504954b5fb917986321291ef94f8c
Trismegiste <florent.g@allopneus.com>
2013-06-18 20:43:41 +0200

    test gallery listing

--- tests/Trismegiste/ControllerTest.php
+++ tests/Trismegiste/ControllerTest.php
@@ -23,6 +23,7 @@ class ControllerTest extends WebTestCase
         $result = $crawler->filterXPath('//div/a[@id]/img');
         $this->assertCount(1, $result);
         $this->assertEquals('/fixtures/photo/1/category.png', $result->attr('src'));
+        $this->assertRegExp('#img\.jpg#', $client->getResponse()->getContent());
     }
 
     public function createApplication()
