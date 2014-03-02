a28de07cc5731e83bb300f9e369ff4b107fbb45b
Trismegiste <flo@trismegiste.net>
2014-03-02 04:43:15 +0100

    denying google bot

--- src/Trismegiste/Controller.php
+++ src/Trismegiste/Controller.php
@@ -14,6 +14,10 @@ class Controller
 
     public function home(Request $request, Application $app)
     {
+        if (preg_match('#googlebot#i', $request->server->get('HTTP_USER_AGENT'))) {
+            $app->abort(404);
+        }
+
         $param['category'] = $app->getRepo()->findCategory();
         $param['gallery'] = $app->getRepo()->findAll();
 
